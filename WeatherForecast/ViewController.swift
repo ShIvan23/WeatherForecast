0377f9456403aeb7e9d0528c61837153277aad83
Ivan Shishkin <ivan23-1@yandex.ru>
2021-05-18 10:08:36 +0300

    Add initial project

---
+++ WeatherForecast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by Ivan on 18.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .purple
+    }
+
+
+}
+
